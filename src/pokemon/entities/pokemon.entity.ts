c4e2a6b7a7b7d9cc4589b2fb893343a9efbcbe5d
Daniel Meza <danielmeza.4c@gmail.com>
2023-02-24 22:11:36 -0700

    definición de esquema y configuración de bd

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,21 @@
-export class Pokemon {}
+import { Schema } from '@nestjs/mongoose';
+import { Prop, SchemaFactory } from '@nestjs/mongoose/dist';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  // id: string; Mongo ya lo crea y lo proporciona sin declararlo
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
