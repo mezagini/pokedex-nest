a9c67e10402afcba11405718b201945e71247606
Daniel Meza <danielmeza.4c@gmail.com>
2023-04-11 17:36:05 -0700

    inserciónde múltiples pokemons por lote

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from "axios";
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
+
+  ) {}
+  
+  
+  async executeSeed() {
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    //* Obtener nombre y id del pokemon
+    data.results.forEach( async ({name, url}) => {
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2];
+
+      const pokemon = await this.pokemonModel.create({ name, no });
+    });
+
+    return 'Seed Executed';
+  }
+}
